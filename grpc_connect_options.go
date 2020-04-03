d928a150672f97c99945f3368940a258d647f12b
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2020-04-03 10:04:57 +0700

    feature: add circuit breaker and retry wrapper for grpc UnaryClientInterceptor (#5)
    * WIP
    
    * fix missing channel set
    
    * feature: add circuit breaker and retry wrapper for grpc UnaryClientInterceptor
    
    * change log level to debug for success

---
+++ grpc_connect_options.go
@@ -0,0 +1,103 @@
+package connect
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc/metadata"
+
+	"google.golang.org/grpc"
+
+	"github.com/imdario/mergo"
+	"github.com/kumparan/go-utils"
+)
+
+// GRPCUnaryInterceptorOptions wrapper options for the grpc connection
+type GRPCUnaryInterceptorOptions struct {
+	// UseCircuitBreaker flag if the connection will implement a circuit breaker
+	UseCircuitBreaker bool
+
+	// RetryCount retry the operation if found error.
+	// When set to <= 1, then it means no retry
+	RetryCount int
+
+	// RetryInterval next interval for retry.
+	RetryInterval time.Duration
+
+	// Timeout value, will return context deadline exceeded when the operation exceeds the duration
+	Timeout time.Duration
+
+	// Flag if the connection is being pooled. Differences will be impacted in appending metadata to the context
+	IsPooled bool
+}
+
+var defaultGRPCUnaryInterceptorOptions = &GRPCUnaryInterceptorOptions{
+	UseCircuitBreaker: false,
+	RetryCount:        0,
+	RetryInterval:     20 * time.Millisecond,
+	Timeout:           1 * time.Second,
+	IsPooled:          false,
+}
+
+func UnaryClientInterceptor(opts *GRPCUnaryInterceptorOptions) grpc.UnaryClientInterceptor {
+	o := applyGRPCUnaryInterceptorOptions(opts)
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		ctx, cancel := context.WithTimeout(ctx, o.Timeout)
+		defer cancel()
+
+		callSkipCount := 4
+		if o.IsPooled {
+			callSkipCount = 5
+		}
+
+		ctx = metadata.AppendToOutgoingContext(ctx, "caller", utils.MyCaller(callSkipCount))
+
+		if o.UseCircuitBreaker {
+			success := make(chan bool, 1)
+			errC := hystrix.GoC(ctx, method, func(ctx context.Context) error {
+				err := o.retryableInvoke(ctx, method, req, reply, cc, invoker, opts...)
+				if err == nil {
+					success <- true
+				}
+				return err
+			}, nil)
+
+			select {
+			case out := <-success:
+				logrus.Debugf("success %v", out)
+			case err := <-errC:
+				logrus.Warnf("failed %s", err)
+
+				return status.Error(codes.Unavailable, err.Error())
+			}
+		}
+
+		return o.retryableInvoke(ctx, method, req, reply, cc, invoker, opts...)
+	}
+}
+
+func (o *GRPCUnaryInterceptorOptions) retryableInvoke(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	return utils.Retry(o.RetryCount, o.RetryInterval, func() error {
+		err := invoker(ctx, method, req, reply, cc, opts...)
+
+		if status.Code(err) != codes.Unavailable { // stop retrying unless Unavailable
+			return utils.NewRetryStopper(err)
+		}
+		return err
+	})
+}
+
+func applyGRPCUnaryInterceptorOptions(opts *GRPCUnaryInterceptorOptions) *GRPCUnaryInterceptorOptions {
+	if opts == nil {
+		return defaultGRPCUnaryInterceptorOptions
+	}
+	// if error occurs, also return options from input
+	_ = mergo.Merge(opts, *defaultGRPCUnaryInterceptorOptions)
+	return opts
+}
