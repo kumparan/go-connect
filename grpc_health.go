53664d950897cb3db2662d7db9dcc8749680696c
alyandaastri19 <alyandaastri19@gmail.com>
2025-07-29 16:22:41 +0700

    misc: use default health server

--- grpc_health.go
+++ grpc_health.go
@@ -7,15 +7,12 @@ import (
 )
 
 // RegisterHealthCheckService init health check service
-func RegisterHealthCheckService(appName string, registrar grpc.ServiceRegistrar, customHandler grpc_health_v1.HealthServer) {
+func RegisterHealthCheckService(registrar grpc.ServiceRegistrar, customHandler grpc_health_v1.HealthServer) {
 	if customHandler != nil {
 		grpc_health_v1.RegisterHealthServer(registrar, customHandler)
 		return
 	}
 
-	defaultHealthServer := health.NewServer()                                                 // initialize health server
-	defaultHealthServer.SetServingStatus(appName, grpc_health_v1.HealthCheckResponse_SERVING) // set default status
-
-	// use default handler
-	grpc_health_v1.RegisterHealthServer(registrar, defaultHealthServer)
+	// use default health server
+	grpc_health_v1.RegisterHealthServer(registrar, health.NewServer())
 }
